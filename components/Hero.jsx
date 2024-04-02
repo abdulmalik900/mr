6de37f4be856527cbd51071c7eb5a1437c8219c4
abdulmalik900 <amalik90090@gmail.com>
2024-04-02 15:48:41 +0500

    Refactor Hero component styling and import dependencies

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,32 +1,28 @@
-import React from 'react';
-import Image from 'next/image';
+import './hero.css';
+import { MdOutlineArrowOutward } from "react-icons/md";
 
-const Hero = () => {
+function Hero() {
   return (
-    <div className="relative">
-      <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 text-white">
-        <h1 className="text-lg  md:text-2xl lg:text-3xl xl:text-3xl  font-bold">Effortless Moving and Packing</h1>
-      </div>
-{/*  */}
-      <div className="absolute top-1/2 left-1/4 pt-16  md:pt-28 transform -translate-y-1/2 text-white">
-        <h1 className="text-lg  md:text-2xl lg:text-3xl xl:text-3xl  mt-7 lg:mt-7 font-bold">with Transformative Solutions</h1>
-        <div className='transform -rotate-0'>
-          <button className="inline-flex mt-3 items-center px-3 sm:px-4 py-2 sm:py-3 justify-center gap-2 rounded-lg bg-[#0073CE] text-sm sm:text-base">
-            Explore More 
-            <Image src="/icons/arrow.svg" width={10} height={10} />
-          </button>
+    <div
+      className="hero flex items-center px-[10%] w-full h-[60vh] sm:h-[70vh] md:h-[80vh] xl:h-[90vh] bg-no-repeat bg-cover bg-center "
+      style={{ zIndex: "-999" }}
+    >
+      <div className="text-white flex flex-col gap-y-12 ">
+        <div>
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">
+            Effortless Moving and Packing
+          </h1>
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-bold">
+            with Transformative Solutions
+          </h1>
         </div>
+        <button className=" w-fit inline-flex  items-center px-3 sm:px-4 py-2 sm:py-3 justify-center gap-2 rounded-lg bg-[#0073CE] text-sm sm:text-base">
+          Explore More
+          <MdOutlineArrowOutward className="text-xl" />
+        </button>
       </div>
-
-      <Image
-        src="/Hero.png"
-        layout="responsive"
-        width={1917}
-        height={1100}
-        alt="hero"
-      />
     </div>
   );
-};
+}
 
 export default Hero;
